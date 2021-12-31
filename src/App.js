e9734e51efd6408ba2a9d44b86e6ec6d9b4c285d
Holly Henske <hcover333@gmail.com>
2021-12-31 06:32:38 -0600

    first commit, header and beginning of grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Holly Henske, Web Developer</h1>
       </header>
+      <body className="App-body">
+        <div class="grid-container">
+          
+
+        </div>
+        
+
+      </body>
     </div>
   );
 }
